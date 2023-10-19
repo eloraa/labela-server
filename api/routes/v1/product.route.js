fad5fabb29f15fa97e94a3ba2b652690f3649196
elora <108570316+eloraistired@users.noreply.github.com>
2023-10-19 17:07:18 +0100

    fix the bug

--- api/routes/v1/product.route.js
+++ api/routes/v1/product.route.js
@@ -2,13 +2,14 @@ const express = require("express");
 const controller = require("../../controllers/product.controller");
 const validate = require("express-validation");
 const { product } = require("../../validations/product.validation");
+const { pick } = require("lodash");
 
 const router = express.Router();
 
 router
     .route("/:id")
 
-    .get(validate(product), controller.get);
+    .get(validate(pick(product, 'params')), controller.get);
 
 router
     .route("/add")
