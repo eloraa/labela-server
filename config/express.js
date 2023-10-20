3af25535a839b57334e8946959a053ab9d65dd59
elora <108570316+eloraistired@users.noreply.github.com>
2023-10-20 18:14:59 +0100

    added the favicon

--- config/express.js
+++ config/express.js
@@ -1,16 +1,14 @@
-Promise = require('bluebird')
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const compress = require('compression');
-const methodOverride = require('method-override');
-const cors = require('cors');
-const helmet = require('helmet');
-const routes = require('../api/routes/v1');
-const {
-    logs
-} = require('./vars');
-const error = require('../api/middlewares/error');
+Promise = require("bluebird");
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const compress = require("compression");
+const methodOverride = require("method-override");
+const cors = require("cors");
+const helmet = require("helmet");
+const routes = require("../api/routes/v1");
+const { logs, favicon } = require("./vars");
+const error = require("../api/middlewares/error");
 
 /**
  * Express instance
@@ -22,9 +20,11 @@ app.use(morgan(logs));
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 // gzip compression
 app.use(compress());
@@ -39,10 +39,14 @@ app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
-
 // mount api v1 routes
-app.use('/v1', routes)
+app.use("/v1", routes);
 
+// send the favicon if requested
+app.get("/favicon.ico", (req, res) => {
+    res.set("Content-Type", "image/svg+xml");
+    res.send(favicon);
+});
 // if error is not an instanceOf APIError, convert it.
 app.use(error.converter);
 
@@ -51,6 +55,5 @@ app.use(error.notFound);
 
 // error handler, send stacktrace only during development
 app.use(error.handler);
-
 
 module.exports = app;
